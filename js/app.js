9a3da7ab7eddab9a0e5ee076d232c184b62b4ebf
samuel addisu <samueladdisu7@gmail.com>
2021-07-10 12:12:58 +0300

    Ipad design

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 const navToggle = document.querySelector('.ham');
 const linksContainer = document.querySelector('.links-container');
-
+const nav = document.querySelector('.nav');
+const logo = document.querySelector('.logo');
+const menu = document.querySelector('.ham')
+window.addEventListener('scroll', ()=>{
+    const navheight = nav.getBoundingClientRect().height;
+    const scorllheight = window.pageYOffset;
+    console.log(scorllheight);
+    if(scorllheight > navheight)
+    {
+        menu.classList.add('black')
+        logo.innerHTML = `<img src="./img/white_logo.svg" alt="Battery World Logo">`
+        nav.classList.add('fixed-nav')
+    }else{
+        logo.innerHTML = `<img src="./img/Battery World.svg" alt="Battery World Logo">`
+        nav.classList.remove('fixed-nav')
+        menu.classList.remove('black')
+    }
+})
 navToggle.addEventListener('click', () =>{
     linksContainer.classList.toggle('show-links');
 })
